11131f1d66696ff415f2ac29127bed095cecdbaa
Daniel Ritchie <daniel.c.ritchie@gmail.com>
2015-10-20 18:22:56 -0700

    Better organization of futures code, + immediate policy. I'm noticing some weird behavior--this'll help me debug what's going on.

--- src/futures.js
+++ src/futures.js
@@ -1,77 +1,98 @@
+var assert = require('assert');
 
 module.exports = function(env) {
 
-	// Stochastic ordering
-	function sfuture(s, k, a, fn)
-	{
+	function makeFuture(s, k, a, fn) {
+		// Create the global futures list, if it does not exist
 		if (s.__futures === undefined)
 			s.__futures = [];
-		var future = function(s, k)
-		{
-			// Use the address from the future's creation point
+		// The future just calls the original function with the address
+		//    from its creation point.
+		var future = function(s, k) {
 			return fn(s, k, a);
 		}
-		s.__futures = s.__futures.slice();
-		s.__futures.push(future);
-		return k(s, future);
+		// Append this future to the global list
+		s.__futures = s.__futures.concat([future]);
+		return k(s);
 	}
 
-	// Deterministic, depth-first ordering
-	function dfuture(s, k, a, fn)
-	{
-		return fn(s, k, a);
-	}
-
-	function future(s, k, a, fn) {
-		// We default to using stochastic futures
-		if (s.__futureFn === undefined) {
-			s.__futureFn = sfuture;
+	var policies = {
+		// Immediate policy: Just run the future immediately.
+		immediate: {
+			future: function(s, k, a, fn) {
+				return fn(s, k, a);
+			},
+			finishAllFutures: function(s, k, a) {
+				return k(s);
+			}
+		},
+		// Deterministic policy: Store futures in a stack, and pull
+		//    futures off of that stack in a deterministic LIFO order.
+		// (This should be semantically equivalent to the immediate policy)
+		deterministic: {
+			future: makeFuture,
+			finishAllFutures: function(s, k, a) {
+				if (s.__futures !== undefined && s.__futures.length > 0) {
+					// Pop off the top
+					var fut = s.__futures[s.__futures.length - 1];
+					s.__futures = s.__futures.slice(0, s.__futures.length - 1);
+					return fut(s, function(s) {
+						return policies.deterministic.finishAllFutures(s, k, a);
+					});
+				}
+				else return k(s);
+			}
+		},
+		// Stochastic policy: Store futures in a list, and pull
+		//    futures out of that list in random order.
+		stochastic: {
+			future: makeFuture,
+			finishAllFutures: function(s, k, a) {
+				if (s.__futures !== undefined && s.__futures.length > 0) {
+					// Pick a random future
+					return sample(s, function(s, i) {
+						var fut = s.__futures[i];
+						s.__futures = s.__futures.slice();
+						s.__futures.splice(i, 1);
+						return fut(s, function(s) {
+							return policies.stochastic.finishAllFutures(s, k, a);
+						});
+					}, a, randomIntegerERP, [s.__futures.length]);
+				}
+				else return k(s);
+			}
 		}
-		return s.__futureFn(s, k, a, fn);
 	}
 
 	// Switch what type of future is being used
 	function setFuturePolicy(s, k, a, policyname) {
-		if (policyname == 'stochastic') {
-			s.__futureFn = sfuture;
-		} else if (policyname == 'deterministic') {
-			s.__futureFn = dfuture;
-		} else
-			throw 'Unknown future policy ' + policyname;
+		assert(policies.hasOwnProperty(policyname));
+		s.__futurePolicy = policies[policyname];
 		return k(s);
 	}
 
-	function forceFuture(s, k, a, future)
-	{
-		var i = s.__futures.indexOf(future);
-		s.__futures = globalStore.__futures.slice();
-		s.__futures.splice(i, 1);
-		return future(s, k);
-	}
-
-	function finishAllFutures(s, k, a)
-	{
-		if (s.__futures !== undefined && s.__futures.length > 0)
-		{
-			return sample(s, function(s, i)
-			{
-				var fut = s.__futures[i];
-				s.__futures = s.__futures.slice();
-				s.__futures.splice(i, 1);
-				return fut(s, function(s)
-				{
-					return finishAllFutures(s, k, a);
-				});
-			}, a, randomIntegerERP, [s.__futures.length]);
+	// We default to the immediate policy
+	function ensurePolicy(s) {
+		if (s.__futurePolicy === undefined) {
+			s.__futurePolicy = policies.immediate;
 		}
-		else return k(s);
 	}
 
+	function future(s, k, a, fn) {
+		ensurePolicy(s);
+		return s.__futurePolicy.future(s, k, a, fn);
+	}
+
+	function finishAllFutures(s, k, a) {
+		ensurePolicy(s);
+		return s.__futurePolicy.finishAllFutures(s, k, a);
+	}
 
 	return {
-		future: future,
 		setFuturePolicy: setFuturePolicy,
-		forceFuture: forceFuture,
+		future: future,
 		finishAllFutures: finishAllFutures
 	}
 }
+
+
