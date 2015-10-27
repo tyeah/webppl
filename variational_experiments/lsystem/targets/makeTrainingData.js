f33ac238278d022df753194f2fd872395ff4c066
Daniel Ritchie <daniel.c.ritchie@gmail.com>
2015-10-27 11:58:22 -0700

    Store all images as grayscale, to save space.

--- variational_experiments/lsystem/targets/makeTrainingData.js
+++ variational_experiments/lsystem/targets/makeTrainingData.js
@@ -7,16 +7,18 @@ var trainingDir = __dirname + '/training';
 
 var SMALL_SIZE = 50;
 
-var imgs = fs.readdirSync(sourceDir);
+var imgs = fs.readdirSync(sourceDir).filter(function(filename) {
+	return filename.slice(-4) === '.png'; 
+});
 for (var i = 0; i < imgs.length; i++) {
 	var img = imgs[i];
 	console.log('Converting ' + img + '...');
 	var sourcename = sourceDir + '/' + img;
 	var trainingname = trainingDir + '/' + img;
-	syscall(util.format('convert %s -resize %dx%d png24:%s',
+	syscall(util.format('convert %s -resize %dx%d -colorspace Gray %s',
 		sourcename, SMALL_SIZE, SMALL_SIZE, trainingname));
 	var flopname = util.format('%s/%s_flop.png', trainingDir, img.slice(0, -4));
-	syscall(util.format('convert %s -flop png24:%s',
+	syscall(util.format('convert %s -flop %s',
 		trainingname, flopname));
 }
 console.log('DONE.');
