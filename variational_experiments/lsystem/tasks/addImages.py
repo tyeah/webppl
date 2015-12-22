01bdcebfd634293701251c4bad09e3a23ce9406d
Anna <thomasat@stanford.edu>
2015-12-22 12:57:51 -0800

    adding script for creating images.txt

---
+++ variational_experiments/lsystem/tasks/addImages.py
@@ -0,0 +1,31 @@
+"""
+Iterate through images in source directory, add those meeting a condition to images.txt file.
+
+Usage: addImages.py --outputDir dir --endStr str, adds images in targets/source 
+which end with str to dir. 
+
+"""
+import os
+import argparse 
+
+source_dir = '../targets/source/'
+dataset_dir = '../targets/datasets/'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--outputDir')
+parser.add_argument('--endStr')
+args = parser.parse_args()
+
+filelist = os.listdir(source_dir)
+imagelist = ''
+
+if not os.path.exists(dataset_dir + args.outputDir):
+    os.makedirs(dataset_dir + args.outputDir)
+
+for file in filelist:
+	if file.endswith(args.endStr): 
+		imagelist += file + '\n'
+
+image_file = open(dataset_dir + args.outputDir + '/images.txt', 'w')
+image_file.write(imagelist)
+	
